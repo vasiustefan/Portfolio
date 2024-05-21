060e132c005f67d4518b9fc7c948948bdd20f9b7
vasiustefan <stefandimitrie.vasiu@ulbsibiu.ro>
2024-05-21 21:50:33 +0300

    About section

--- src/App.js
+++ src/App.js
@@ -25,54 +25,60 @@ class App extends Component {
           <div className="home__hero">
             <h1>Hey, I'm Stefan</h1>
             <p>I am determined to become a skilled web developer <br/> and actively working towards that goal.</p>
-            <div className="home__a" id="home__a">
+            <div id="home__projects">
               <a href="./#projects">Projects</a>
             </div>
           </div>
           <div className="social__icons">
-            <a href="https://github.com/vasiustefan" target="_blank" rel="noreferrer"><FaGithub className="social__ico"/></a>
-            <a href="https://www.linkedin.com/in/stefan-vasiu-516559264/" target="_blank" rel="noreferrer"><FaLinkedin className="social__ico" /></a>
-            <a href="https://www.facebook.com/profile.php?id=100081885259340" target="_blank" rel="noreferrer"><FaFacebook className="social__ico" /></a>
-            <a href="https://www.instagram.com/stefan.vasiu/" target="_blank" rel="noreferrer"><FaInstagram className="social__ico"/></a>
+            <a href="https://github.com/vasiustefan" target="_blank" rel="noreferrer"><FaGithub className="social__react__icons"/></a>
+            <a href="https://www.linkedin.com/in/stefan-vasiu-516559264/" target="_blank" rel="noreferrer"><FaLinkedin className="social__react__icons" id="linkedin__icon"/></a>
+            <a href="https://www.facebook.com/profile.php?id=100081885259340" target="_blank" rel="noreferrer"><FaFacebook className="social__react__icons" id="facebook__icon"/></a>
+            <a href="https://www.instagram.com/stefan.vasiu/" target="_blank" rel="noreferrer"><FaInstagram className="social__react__icons" id="instagram__icon"/></a>
           </div>
         </div>
 
         <div className="about__section">
-          <h2>
-            About me
+          <div className="about__hero">
+            <h1>About me</h1>
             <span> - </span>
-            <span>asdakdlasdlsakdaslfkaslsfalskfslalafsk</span>
-          </h2>
-          <div>
-            <div>
+            <p>Here you will find more information about me, <br/> what I do, and my current skills mostly in terms of programming and technology</p>
+          </div>
+          <div className="about__info">
+            <div className="about__info__first">
               <h3>Get to know me!</h3>
               <div>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias maxime porro accusantium dicta quia nemo ducimus! Maxime, alias consectetur. Minima, numquam nostrum provident recusandae deleniti labore voluptatum necessitatibus aliquid maxime.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias maxime porro accusantium dicta quia nemo ducimus! Maxime, alias consectetur. Minima, numquam nostrum provident recusandae deleniti labore voluptatum necessitatibus aliquid maxime.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias maxime porro accusantium dicta quia nemo ducimus! Maxime, alias consectetur. Minima, numquam nostrum provident recusandae deleniti labore voluptatum necessitatibus aliquid maxime.</p>
+                <p>I am a serious and focused <b>Web Developement Enthusiast</b> who is eager to learn and improve himself.</p>
+                <p>Although I may not know a lot currently, <b>I am determined</b> to become a skilled web developer and <b>actively working</b> towards that goal.</p>
+                <p>My <b>dedication</b> and <b>willingness</b> to learn are admirable traits that will undoubtedly help me achieve my ambitions.</p>
               </div>
-              <a href="./#contact">Contact like a button in CSS</a>
+              <div id="about__contact">
+                <a href="./#contact">Contact</a>
+              </div>
             </div>
-            <div>
+            <div className="about__info__second">
               <h3>My skills</h3>
-              <div>
-                <div>HTML</div>
-                <div>CSS</div>
-                <div>JavaScript</div>
-                <div>React.JS</div>
-                <div>Vue.JS</div>
+              <div className="skills__tree">
+                <div className="skills">HTML</div>
+                <div className="skills">CSS</div>
+                <div className="skills">JavaScript</div>
+                <div className="skills">React.JS</div>
+                <div className="skills">Vue.JS</div>
+                <div className="skills">GIT</div>
+                <div className="skills">GitHub</div>
+                <div className="skills">Responsive Design</div>
+                <div className="skills">Leadership</div>
+                <div className="skills">Time Management</div>
               </div>
             </div>
           </div>
         </div>
 
-        <section id="projects">
+        <section className="projects__section">
           <div>
-            <div>
-                <h2>Projects
-                  <span> - </span>
-                  <span>Here you will find some of the personal projects that I created, with each project containing it's own case study</span>
-                </h2>
+            <div className="projects__hero">
+              <h1>Projects</h1>
+              <span> - </span>
+              <p>Here you will find some of the personal projects that I created,<br/> with each project containing it's own case study</p>
             </div>
             <div className="project_content">
               <div className="project_row">
